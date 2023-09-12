32338db243c6ceac4a2ba8dc4c245342e7fd417a
anamulhaque378732 <anamulhaque46312gmail.com>
2023-09-12 17:14:26 +0600

    header and main complete

---
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,13 @@
+ 
+ 
+ 
+ const Bookmarks = () => {
+    return (
+        <div className="md:w-1/3">
+           <h1>book marks
+            </h1> 
+        </div>
+    );
+ };
+ 
+ export default Bookmarks;
